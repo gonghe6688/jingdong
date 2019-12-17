7d4c9c24ffd5459a8bf2c5cd11be240d80488954
gonghe6688 <2725638816@qq.com>
2019-12-17 14:49:19 +0800

    login12.17下午完成'登录页面'

--- src/js/login.js
+++ src/js/login.js
@@ -1,4 +1,17 @@
-let _al = document.querySelectorAll('.denglu > p');
-let _qiehuan = document.querySelector('.qiehuan');
-let _a_qiehuan = document.querySelector('a-qiehuan');
-
+let _al = document.querySelectorAll('ul > li');
+let _ak = document.querySelectorAll('ol > li')
+//我要给每一个p绑定一个点击事件
+for (let i = 0; i < _al.length; i++) {
+    _al[i].setAttribute('index', i);
+    _al[i].onclick = function () {
+        for (let j = 0; j < _al.length; j++) {
+            _al[j].className = '';
+        }
+        for ( let k = 0; k < _ak.length; k++) {
+            _ak[k].className = '';
+        }
+        this.className = 'binda';
+        let index = this.getAttribute('index') - 0;
+        _ak[index].className = 'binda';
+    }
+}
