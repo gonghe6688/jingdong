38b337a3e3bdade9e8d2f45ebd6080c66a6401ad
gonghe6688 <2725638816@qq.com>
2019-12-18 22:20:06 +0800

    12.18今天是一个高兴的日子，老子会动态渲染页面了！啊哈哈哈

---
+++ src/js/dongtai.js
@@ -0,0 +1,22 @@
+let arr = [
+    {
+
+    },
+    {},
+    {},
+    {},
+]
+let box = document.getElementById('box');
+let div = document.createElement('div');
+// div.style.class = 'a-banqu';
+div.style.height = '500px';
+div.style.width = '1150px';
+div.style.border = '1px solid #000';
+div.style.margin = 'auto';
+box.appendChild(div);
+let div_a$kuan = document.createElement('div');
+div_a$kuan.style.width = '213px';
+div_a$kuan.style.height = '500px';
+div_a$kuan.style.float = 'left';
+div_a$kuan.style.backgroundColor = 'rgb(156,191,226)';
+div.appendChild(div_a$kuan)
